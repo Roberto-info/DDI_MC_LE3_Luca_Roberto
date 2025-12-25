722d012ef0856730602759f800d1b7b345c78236
roberto <roberto.fazekas.ges@gmail.com>
2025-12-25 14:27:38 +0100

    Added mc

--- python-client/libs/fetch_data.py
+++ python-client/libs/fetch_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def fetch_data(PARKING_URL, RAIN_URL):
+def fetch_data(PARKING_URL, RAIN_URL, already_installed):
     # Laden ohne Parameter
     df_parking = pd.read_csv(PARKING_URL, sep=";")
     df_rain = pd.read_csv(RAIN_URL, sep=";")
